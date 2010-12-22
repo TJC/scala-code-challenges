88e8f03cc363e86ed45c0ba1fe9ae9f6e30aa77c
Toby Corkindale <tobyc@strategicdata.com.au>
2010-12-22 17:25:00 +1100

    Better isPrime function

--- euler/10/euler10.scala
+++ euler/10/euler10.scala
@@ -2,6 +2,10 @@
 // Find the sum of all the primes below two million.
 import scala.math._
 
-def isPrime(x: Int) : Boolean = !( (2 to ceil(sqrt(x)).toInt).exists(i => x%i == 0) )
+def isPrime(x: Int) : Boolean = x match {
+    case 1 => false
+    case 2 => true
+    case _ => !( (2 to ceil(sqrt(x)).toInt).exists(i => x%i == 0) )
+}
 
 println( (2 to 1999999).filter(isPrime).reduceLeft(_+_) )
--- euler/7/euler7.scala
+++ euler/7/euler7.scala
@@ -3,7 +3,11 @@
 
 import scala.math._
 
-def isPrime(x: Int) : Boolean = !( (2 to ceil(sqrt(x)).toInt).exists(i => x%i == 0) )
+def isPrime(x: Int) : Boolean = x match {
+    case 1 => false
+    case 2 => true
+    case _ => !( (2 to ceil(sqrt(x)).toInt).exists(i => x%i == 0) )
+}
 
 var i = 1
 var n = 1
