dfe94c4877c6018c3aee8534fa29c70fcdc52b1b
Toby Corkindale <tjc@wintrmute.net>
2011-01-10 19:47:32 +1100

    neaten up solution (doesn't need BigInt)

--- euler/10/euler10.scala
+++ euler/10/euler10.scala
@@ -2,16 +2,11 @@
 // Find the sum of all the primes below two million.
 import scala.math._
 
-def isPrime(x: Int) : Boolean = x match {
+def isPrime(x: Long) : Boolean = x match {
     case 1 => false
     case 2 => true
-    case _ => !( (2 to ceil(sqrt(x)).toInt).exists(i => x%i == 0) )
+    case _ => !( (2L to ceil(sqrt(x)).toLong).exists(i => x%i == 0) )
 }
 
-// Need to find a better way of handling this; if I call it the same
-// name as isPrime, but try to dispatch to it with toInt, then it goes
-// recursive.
-def isBigPrime(x: BigInt) : Boolean = isPrime(x.toInt)
-
-println( (BigInt(2) to BigInt(1999999)).filter(isBigPrime).reduceLeft(_+_) )
+println( (2L to 1999999L).filter(isPrime).reduceLeft(_+_) )
 
