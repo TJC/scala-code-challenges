d96d9b30386f7ff8f186b65ad1f80f22afe57dc9
Toby Corkindale <tjc@wintrmute.net>
2012-02-29 15:55:56 +1100

    refactored to use immutable Set

--- scala-melb/001_happy_numbers/happy.scala
+++ scala-melb/001_happy_numbers/happy.scala
@@ -7,32 +7,19 @@ Do not use variables.
 
 */
 
-import scala.collection.mutable
-
 def square_digits(n: Int): Int =
     n.toString.toCharArray.map(_.toString.toInt).map(m => m*m).sum
 
-
 def isHappy(n: Int): Boolean = {
 
-    val seen_map :mutable.Map[Int,Boolean] = mutable.Map()
-
-    def have_seen(m: Int): Boolean = {
-        if (seen_map contains m) return true
-        seen_map(m) = true
-        false
-    }
-
-    def internal_isHappy(m: Int): Boolean = {
-        println(m)
+    def inner(m: Int, seen: Set[Int]): Boolean =
         m match {
             case 1 => true
-            case m if (m < 100 && have_seen(m)) => false
-            case _ => internal_isHappy(square_digits(m))
+            case m if (m < 100 && seen.contains(m)) => false
+            case _ => inner(square_digits(m), seen + m)
         }
-    }
 
-    internal_isHappy(n)
+    inner(n, Set[Int]())
 }
 
 // vim: set ts=4 sw=4 et:
